f69a0c53ffef5ccd13d93deb9174031e991f1589
Mathieu Gordon <mathieu.gordon@gmail.com>
2014-12-16 22:06:16 +0000

    adds move validations

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,4 +1,20 @@
 class Move < ActiveRecord::Base
   belongs_to :match
   belongs_to :player, class_name: "User"
+
+  validates :cell, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 8 }
+  validates :cell, uniqueness: { scope: :match_id }
+
+  validate :marker_is_x_or_o, on: :create
+
+  validate :current_player, on: :create
+
+  def marker_is_x_or_o
+    errors.add(:marker, "should be X or O") unless marker == "X" || marker == "O"
+  end
+
+  def current_player
+    errors.add(:player_id, "is not the current player") unless player_id == match.next_player.id
+  end
+
 end

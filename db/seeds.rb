0de60f9bed6dcdace21d01e30eee85ec2bbca60b
Mathieu Gordon <mathieu.gordon@gmail.com>
2014-12-17 15:15:20 +0000

    adds computer to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.delete_all
+u0 = User.create!(email: "computer@mailinator.com", password: "password", role: "computer", name: "Computer", profile: "Most Powerful Computer on Earth")
 u1 = User.create!(email: "admin@mailinator.com", password: "password", role: "admin", name: "AdminGuy", profile: "admin profile")
 u2 = User.create!(email: "bob@mailinator.com", password: "password", name: "BOB", profile: "bobs profile")
 u3 = User.create!(email: "fred@mailinator.com", password: "password", name: "Freddy", profile: "fred is best")

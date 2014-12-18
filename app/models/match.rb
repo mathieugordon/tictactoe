f0235f42d818779f73d776fa39037fb8c8b29249
Mathieu Gordon <mathieu.gordon@gmail.com>
2014-12-18 13:31:39 +0000

    refinement of match model

--- app/models/match.rb
+++ app/models/match.rb
@@ -8,33 +8,42 @@ class Match < ActiveRecord::Base
   # core game
 
   def board() [0,1,2,3,4,5,6,7,8] end
+
   def winning_combinations() [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]] end
 
-  def populated_board()
+  def populated_board
     a = board
     moves.each { |move| a[move.cell] = move.marker }
     return a
   end    
 
   def check!
-    check_finished!
-    if next_player_is_computer?
+    if won? then set_won!
+    elsif drawn? then set_drawn!
+    elsif next_player_is_computer?
       auto_play!
       reload
-      check_finished!
+      if won? then set_won!
+      elsif drawn? then set_drawn!
+      end
     end
   end
 
-  def check_finished!
-    set_won! if won?
-    set_drawn! if drawn?
+  def set_won!
+    self.update(status: "won", winning_player_id: last_player.id, losing_player_id: next_player.id)
   end
 
-  def set_won!() self.update(status: "won", winning_player_id: last_player.id, losing_player_id: next_player.id) end
-  def set_drawn!() self.update(status: "drawn") end
+  def set_drawn!
+    self.update(status: "drawn")
+  end
 
-  def won?() won_by?(player_x) || won_by?(player_o) end
-  def drawn?() moves.count == 9 end
+  def won?
+    won_by?(player_x) || won_by?(player_o)
+  end
+
+  def drawn?
+    moves.count == 9
+  end
 
   def won_by?(player)
     winning_combinations.any? do |combination|
@@ -44,15 +53,37 @@ class Match < ActiveRecord::Base
     end
   end
 
-  def cell_occupied_by_player?(cell, player) populated_board[cell] == marker(player) end  
-  def cell_occupied?(cell) cell_occupied_by_player?(cell, player_x) || cell_occupied_by_player?(cell, player_o) end
-  def cell_empty?(cell) !cell_occupied?(cell) end
+  def cell_occupied_by_player?(cell, player)
+    populated_board[cell] == marker(player)
+  end
 
-  def last_move() moves.last end
-  def last_player() if last_move.nil? then false else last_move.player end end
-  def next_player() last_player == false || last_player == player_o ? player_x : player_o end
-  def opponent(player) player == player_x ? player_o : player_x end
-  def marker(player) player == player_x ? "X" : "O" end
+  def cell_occupied?(cell)
+    cell_occupied_by_player?(cell, player_x) || cell_occupied_by_player?(cell, player_o)
+  end
+
+  def cell_empty?(cell)
+    !cell_occupied?(cell)
+  end
+
+  def last_move
+    moves.last
+  end
+
+  def last_player
+    last_move.nil? ? false : last_move.player
+  end
+
+  def next_player
+    last_player == false || last_player == player_o ? player_x : player_o
+  end
+
+  def opponent(player)
+    player == player_x ? player_o : player_x
+  end
+
+  def marker(player)
+    player == player_x ? "X" : "O"
+  end
 
   # AI
 
@@ -60,18 +91,31 @@ class Match < ActiveRecord::Base
     Move.create(match_id: self.id, player_id: next_player.id, cell: auto_move(next_player), marker: marker(next_player))
   end
 
-  def next_player_is_computer?() next_player.role?(:computer) end
+  def next_player_is_computer?
+    next_player.role?(:computer)
+  end
 
   def auto_move(player)
     winning_move(player) || blocking_move(player) || progressive_move(player) || random_move
   end
 
-  def winning_move(player) possible_cell(player, 2) end
-  def blocking_move(player) possible_cell(opponent(player), 2) end
-  def progressive_move(player) possible_cell(player, 1) end
-  def random_move() remaining_moves.shuffle.first end
+  def winning_move(player)
+    possible_cell(player, 2)
+  end
 
-  def remaining_moves()
+  def blocking_move(player)
+    possible_cell(opponent(player), 2)
+  end
+
+  def progressive_move(player)
+    possible_cell(player, 1)
+  end
+
+  def random_move
+    remaining_moves.shuffle.first
+  end
+
+  def remaining_moves
     populated_board.select { |cell| cell.is_a? Integer }
   end
 
@@ -90,8 +134,13 @@ class Match < ActiveRecord::Base
 
   # output
 
-  def next_player?(player) player == next_player end
-  def winning_player?(player) player == winning_player end
+  def next_player?(player)
+    player == next_player
+  end
+
+  def winning_player?(player)
+    player == winning_player
+  end
 
   def short_description(player)
     case status

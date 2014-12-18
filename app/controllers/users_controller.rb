6c62bae95a45308595689acfb4e72ef7bec498e7
Mathieu Gordon <mathieu.gordon@gmail.com>
2014-12-18 13:56:36 +0000

    gets ransack working along with sort

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,8 @@ class UsersController < ApplicationController
   load_and_authorize_resource
 
   def index
-    # @users = User.all.sort_by(&:wins).reverse
     @q = User.search(params[:q])
-    @users = @q.result(distinct: true)
+    @users = @q.result(distinct: true).sort_by(&:wins).reverse
   end
 
   def show

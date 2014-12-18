f6c692e965f4ea264c895c270fb51b0d20b728b6
Mathieu Gordon <mathieu.gordon@gmail.com>
2014-12-18 16:35:35 +0000

    adds default for match status

--- db/migrate/20141216213626_create_matches.rb
+++ db/migrate/20141216213626_create_matches.rb
@@ -3,7 +3,7 @@ class CreateMatches < ActiveRecord::Migration
     create_table :matches do |t|
       t.integer :player_x_id
       t.integer :player_o_id
-      t.string :status
+      t.string :status, :default => "in progress"
       t.integer :winning_player_id
       t.integer :losing_player_id
 

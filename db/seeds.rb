2ad456e4f067e43a3e103e98f0caefad5723e661
Mathieu Gordon <mathieu.gordon@gmail.com>
2014-12-18 18:49:31 +0000

    amends seed data to use remote image urls

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.delete_all
-u0 = User.create!(email: "computer@mailinator.com", password: "password", role: "computer", name: "Computer", profile_text: "Most Powerful Computer on Earth")
-u1 = User.create!(email: "admin@mailinator.com", password: "password", role: "admin", name: "Admin", profile_text: "Best Admin on Earth")
+u0 = User.create!(email: "computer@mailinator.com", password: "password", role: "computer", name: "Computer", profile_text: "Most Powerful Computer on Earth", remote_user_image_url: "http://i.imgur.com/jpE8bEJ.gif")
+u1 = User.create!(email: "admin@mailinator.com", password: "password", role: "admin", name: "Admin", profile_text: "Best Admin on Earth", remote_user_image_url: "http://i.imgur.com/jpE8bEJ.gif")

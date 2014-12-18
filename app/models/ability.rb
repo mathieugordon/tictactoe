ca1b9d7696e18e61e7948a9ecb1cb5fee5af18a1
Mathieu Gordon <mathieu.gordon@gmail.com>
2014-12-18 19:36:46 +0000

    edits abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,13 +4,16 @@ class Ability
   def initialize(user)
     user ||= User.new
     if user.role? :admin
-        can :manage, :all
+      can :manage, :all
     elsif user.persisted?
-        can :read, :all
-        can :manage, Match
-        can :manage, User, id: user.id
+      can :read, User
+      can :update, User, id: user.id
+      can :create, Match
+      can [:read, :move], Match do |match|
+        match.player_x_id == user.id || match.player_o_id == user.id
+      end
     else
-        can :read, User
+      can :read, User
     end
   end
 

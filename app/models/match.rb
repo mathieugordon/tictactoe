64665f27607b39759cb7f6010b3ce642f8fe93ef
Mathieu Gordon <mathieu.gordon@gmail.com>
2014-12-18 00:13:06 +0000

    simplifies rendering of match

--- app/models/match.rb
+++ app/models/match.rb
@@ -13,9 +13,9 @@ class Match < ActiveRecord::Base
   # check game
 
   def check_and_update!
-    if check_if_won?
+    if won?
       self.update(status: "won", winning_player_id: last_player.id, losing_player_id: next_player.id)
-    elsif check_if_drawn?
+    elsif drawn?
       self.update(status: "drawn")
     elsif next_player_is_computer?
       Move.create(match_id: self.id, player_id: next_player.id, cell: auto_move(next_player), marker: marker(next_player))
@@ -23,11 +23,11 @@ class Match < ActiveRecord::Base
     end
   end
 
-  def check_if_won? () check_if_won_by?(player_x) || check_if_won_by?(player_o) end
-  def check_if_drawn?() moves.count == 9 end
+  def won? () won_by?(player_x) || won_by?(player_o) end
+  def drawn?() moves.count == 9 end
   def next_player_is_computer?() next_player.role?(:computer) end
 
-  def check_if_won_by?(player)
+  def won_by?(player)
     winning_combinations.any? do |combination|
       combination.all? do |cell|
         occupied_and_matches?(cell, player)
@@ -45,8 +45,8 @@ class Match < ActiveRecord::Base
   def last_move() moves.last end
   def last_player() if last_move.nil? then false else last_move.player end end
   def next_player() last_player == false || last_player == player_o ? player_x : player_o end
-  def opposite_player(player) player == player_o ? player_x : player_o end
-  def marker(player) player == player_o ? "O" : "X" end
+  def opponent(player) player == player_x ? player_o : player_x end
+  def marker(player) player == player_x ? "X" : "O" end
 
   # AI
 
@@ -55,7 +55,7 @@ class Match < ActiveRecord::Base
   end
 
   def winning_move(player) possible_cell(player, 2) end
-  def blocking_move(player) possible_cell(opposite_player(player), 2) end
+  def blocking_move(player) possible_cell(opponent(player), 2) end
   def progressive_move(player) possible_cell(player, 1) end
   def random_move() remaining_moves.shuffle.first end
 
@@ -84,63 +84,38 @@ class Match < ActiveRecord::Base
     end
   end
 
-  # output helper methods
-
-  def in_progress?() status == "in progress" end
-  def won?() status == "won" end
-  def drawn?() status == "drawn" end
+  # output methods
 
   def next_player?(player) player == next_player end
   def winning_player?(player) player == winning_player end
-  def losing_player?(player) player == losing_player end
 
   def short_description(player)
-    if in_progress?
-      if next_player?(player) then "your turn"
-      else "#{next_player.name}'s turn"
-      end
-    elsif won?
-      "won by #{winning_player.name}!"
-    elsif drawn?
-      "draw"
+    case status
+    when "in progress" then next_player?(player) ? "your turn" : "waiting for opponent"
+    when "won" then winning_player?(player) ? "won" : "lost"
+    when "drawn" then "draw"
     end
   end
 
   def long_description(player)
-    if in_progress?
-      if next_player?(player) then "Game in progress - your turn!"
-      else "Game in progress - #{next_player.name}'s turn!"
-      end
-    elsif won?
-      "Game over - won by #{winning_player.name}!"
-    elsif drawn?
-      "Game over - draw!"
+    case status
+    when "in progress" then next_player?(player) ? "Your turn!" : "Waiting for opponent!"
+    when "won" then winning_player?(player) ? "Game over - won!" : "Game over - lost!"
+    when "drawn" then "Game over - draw!"
     end
   end
 
   def player_indicator(player)
-    if in_progress?
-      if next_player?(player) then "⬅ next player"
-      else ""
-      end
-    elsif won?
-      if winning_player?(player) then "⬅ winner"
-      elsif losing_player?(player) then "⬅ loser"
-      end
-    elsif drawn?
-      ""
+    case status
+    when "in progress" then next_player?(player) ? "⬅ next player" : ""
+    when "won" then winning_player?(player) ? "⬅ winner" : "⬅ loser"
     end
   end
 
   def player_tag(player)
-    if in_progress?
-      ""
-    elsif won?
-      if winning_player?(player) then "good"
-      elsif losing_player?(player) then "bad"
-      end
-    elsif drawn?
-      "neutral"
+    case status
+    when "won" then winning_player?(player) ? "good" : "bad"
+    when "drawn" then "neutral"
     end
   end
 
